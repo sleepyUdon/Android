2a6dbf68007171f5db55a48dfcccad85c9d37e7e
Viviane Chan <vivianechan@Vivianes-MacBook-Pro.local>
2017-07-03 21:24:09 -0400

    WIP

--- app/src/main/java/ca/interfaced/dockmaster/ProjectDescription_Activity.java
+++ app/src/main/java/ca/interfaced/dockmaster/ProjectDescription_Activity.java
@@ -3,8 +3,7 @@ package ca.interfaced.dockmaster;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
 
 import java.util.UUID;
 
