d7cd09548e194b5ce39e3af7ce32974c8bc85d35
Viviane Chan <vivianechan@Vivianes-MacBook-Pro.local>
2017-07-26 08:22:50 -0400

    refactored recyler view

--- app/src/main/java/ca/interfaced/dockmaster/Model/Project.java
+++ app/src/main/java/ca/interfaced/dockmaster/Model/Project.java
@@ -13,59 +13,60 @@ import io.realm.annotations.PrimaryKey;
  * Created by vivianechan on 2017-06-30.
  */
 
-public class Project extends RealmObject {
+public class Project  {
     @PrimaryKey
-    private long id;
-    private String projectName;
-    private String ProjectAddress;
-    private RealmList<Asset>assets;
-    private RealmList<User>users;
+    private UUID mId;
+    private String mProjectName;
+    private String mProjectAddress;
+    private RealmList<Asset>mAssets;
+    private RealmList<User>mUsers;
 
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getProjectName() {
+        return mProjectName;
+    }
+
+    public void setProjectName(String projectName) {
+        mProjectName = projectName;
+    }
+
+    public String getProjectAddress() {
+        return mProjectAddress;
+    }
+
+    public void setProjectAddress(String projectAddress) {
+        mProjectAddress = projectAddress;
+    }
+
+    public RealmList<Asset> getAssets() {
+        return mAssets;
+    }
+
+    public void setAssets(RealmList<Asset> assets) {
+        mAssets = assets;
+    }
+
+    public RealmList<User> getUsers() {
+        return mUsers;
+    }
+
+    public void setUsers(RealmList<User> users) {
+        mUsers = users;
+    }
 //    private String ProjectStatus;
 //    private Date mDate;
 //    private List<User> mUsers = new ArrayList<>();
 
     // TODO: project image
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getProjectAddress() {
-        return ProjectAddress;
-    }
-
-    public void setProjectAddress(String projectAddress) {
-        ProjectAddress = projectAddress;
-    }
-
-    public RealmList<Asset> getAssets() {
-        return assets;
-    }
-
-    public void setAssets(RealmList<Asset> assets) {
-        this.assets = assets;
-    }
-
-    public RealmList<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(RealmList<User> users) {
-        this.users = users;
-    }
 
 
 
--- app/src/main/java/ca/interfaced/dockmaster/Projectslist_Fragment.java
+++ app/src/main/java/ca/interfaced/dockmaster/Projectslist_Fragment.java
@@ -8,27 +8,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
+import android.widget.TextView;
 
+import java.util.List;
 
+import ca.interfaced.dockmaster.Model.Project;
+import ca.interfaced.dockmaster.Model.Projects;
 
 
 public class Projectslist_Fragment extends Fragment {
 
-    private static final String TAG = "RecyclerViewFragment";
-    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
-    private static final int SPAN_COUNT = 2;
-    private static final int DATASET_COUNT = 60;
-
-    private enum LayoutManagerType {
-        LINEAR_LAYOUT_MANAGER
-    }
-
-    protected LayoutManagerType mCurrentLayoutManagerType;
-    protected RecyclerView mRecyclerView;
-    protected CustomAdapter mAdapter;
-    protected RecyclerView.LayoutManager mLayoutManager;
-    protected String[] mDataset;
+    private RecyclerView mProjectRecyclerView;
+    private ProjectAdapter mAdapter;
 
     public static ca.interfaced.dockmaster.Projectslist_Fragment newInstance() {
         return new ca.interfaced.dockmaster.Projectslist_Fragment();
@@ -38,82 +29,96 @@ public class Projectslist_Fragment extends Fragment {
         // Required empty public constructor
     }
 
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+    }
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Initialize dataset, this data would usually come from a local content provider or
-        // remote server.
-        initDataset();
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.projectslist_fragment, container, false);
-        rootView.setTag(TAG);
+        View view = inflater.inflate(R.layout.projectslist_fragment, container, false);
 
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        mProjectRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        mProjectRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        // LinearLayoutManager is used here, this will layout the elements in a similar fashion
-        // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
-        // elements are laid out.
-        mLayoutManager = new LinearLayoutManager(getActivity());
+        updateUI();
 
-        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
-
-        if (savedInstanceState != null) {
-            // Restore saved layout manager type.
-            mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
-                    .getSerializable(KEY_LAYOUT_MANAGER);
-        }
-        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
-
-        mAdapter = new CustomAdapter(mDataset);
-        // Set CustomAdapter as the adapter for RecyclerView.
-        mRecyclerView.setAdapter(mAdapter);
-
-                setRecyclerViewLayoutManager(LayoutManagerType.LINEAR_LAYOUT_MANAGER);
-
-        return rootView;
+        return view;
     }
 
-    /**
-     * Set RecyclerView's LayoutManager to the one given.
-     *
-     * @param layoutManagerType Type of layout manager to switch to.
-     */
-    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
-        int scrollPosition = 0;
 
-        // If a layout manager has already been set, get current scroll position.
-        if (mRecyclerView.getLayoutManager() != null) {
-            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
-                    .findFirstCompletelyVisibleItemPosition();
+    private void updateUI() {
+        Projects projects = Projects.get(getActivity());
+        List<Project> projectList = projects.getProjects();
+
+        mAdapter = new ProjectAdapter(projectList);
+        mProjectRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    private class ProjectHolder extends RecyclerView.ViewHolder {
+        private Project mProject;
+
+        public void bindProject(Project project) {
+            mProject = project;
+            mProjectNameTextView.setText(mProject.getProjectName());
+            mProjectAddressTextView.setText(mProject.getProjectAddress());
         }
 
-                mLayoutManager = new LinearLayoutManager(getActivity());
-                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        public TextView mProjectNameTextView;
+        public TextView mProjectAddressTextView;
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
-//        mRecyclerView.scrollToPosition(scrollPosition);
+
+        public ProjectHolder(View itemView) {
+            super(itemView);
+            // Define click listener for the ViewHolder's View.
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+            mProjectNameTextView = (TextView) itemView.findViewById(R.id.project_name);
+            mProjectAddressTextView = (TextView) itemView.findViewById(R.id.project_address);
+
+        }
+
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        // Save currently selected layout manager.
-        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
-        super.onSaveInstanceState(savedInstanceState);
-    }
 
-    /**
-     * Generates Strings for RecyclerView's adapter. This data would usually come
-     * from a local content provider or remote server.
-     */
-    private void initDataset() {
-        mDataset = new String[DATASET_COUNT];
-        for (int i = 0; i < DATASET_COUNT; i++) {
-            mDataset[i] = "Project #" + i;
+    private class ProjectAdapter extends RecyclerView.Adapter<ProjectHolder> {
+        private List<Project> mProjects;
+
+        public ProjectAdapter(List<Project> projects) {
+            mProjects = projects;
+        }
+
+        @Override
+        public ProjectHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.project_item, parent, false);
+
+            return new ProjectHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ProjectHolder holder, final int position) {
+            Project project = mProjects.get(position);
+            holder.bindProject(project);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mProjects.size();
         }
     }
+
+
 }
